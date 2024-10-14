0d7229b3472fc752933457225d4b934119e91ebb
alver-romero <alverromero11@gmaill.com>
2024-10-14 15:35:15 -0300

    correciones

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { ExpoRouter } from 'expo-router';
 import React from 'react';
+import { ExpoRouter } from 'expo-router';
 
 export default function App() {
+    // @ts-ignore
     return <ExpoRouter />;
 }
